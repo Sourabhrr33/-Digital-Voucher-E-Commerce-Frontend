428c66b0a3918e28d2dafdbe12e998ea84b20240
Sourabh Rathor <Apple@MacBook-Pro.local>
2025-11-04 22:24:40 +0530

    commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // backend URL
+  baseURL: "https://digital-voucher-e-commerce-backend.onrender.com/api", 
 });
 
 api.interceptors.request.use((config) => {
